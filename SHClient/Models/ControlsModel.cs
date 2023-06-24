db72d2122233cce88fc7e0e7caec1d089a45f332
Adham084 <79041023+Adham084@users.noreply.github.com>
2023-06-24 13:18:43 +0300

    controls model

--- SHClient/Models/ControlsModel.cs
+++ SHClient/Models/ControlsModel.cs
@@ -3,12 +3,23 @@
 public struct ControlsModel
 {
     public bool RoomLight { get; set; }
-    public bool GarageLight { get; set; }
     public bool HallLight { get; set; }
+    
+    public bool GarageLight { get; set; }
     public bool FenceLight { get; set; }
 
     public bool GarageDoor { get; set; }
     public bool Fan { get; set; }
 
     public bool Alarm { get; set; }
+    
+    // TimeStamps
+    
+    public long GarageLightTS { get; set; }
+    public long FenceLightTS { get; set; }
+
+    public long GarageDoorTS { get; set; }
+    public long FanTS { get; set; }
+
+    public long AlarmTS { get; set; }
 }
